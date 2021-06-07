9bda1a6614d2830d37bafaeffe74194b63862031
Max <73542409+roswerk@users.noreply.github.com>
2021-06-07 20:18:35 +0200

    Updated nprogress file path

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { mockData } from './mock-data';
 import axios from "axios";
-import NProgress from "./nprogress.css";
+import NProgress from 'nprogress';
 
 export const extractLocations = (events) => {
   var extractLocations = events.map((event) => event.location);
