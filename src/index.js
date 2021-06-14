a502cbcd44480269bec67c93a08ff35f4592823c
Max <73542409+roswerk@users.noreply.github.com>
2021-06-14 18:06:44 +0200

    Linked APM to index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('fc8dad3402de490f8b617f1140e75e81').install();
 
 ReactDOM.render(
   <React.StrictMode>
