39cab51b6e23b980b11d3d78c37e8f0dd7044698
Max <73542409+roswerk@users.noreply.github.com>
2021-06-21 11:46:36 +0200

    Refactored offline PWA feature

--- src/api.js
+++ src/api.js
@@ -29,9 +29,9 @@ export const getEvents = async () => {
   }
 
   if (!navigator.onLine) {
-    const {events} = await localStorage.getItem('lastEvents');
+    const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return {events: JSON.parse(events), locations: extractLocations(events)};
+    return JSON.parse(data).events
   }
 
   const token = await getAccessToken();
