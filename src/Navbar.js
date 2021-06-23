64b19eef7e2b07c266347d513ab7b33cc3ba7dce
Max <73542409+roswerk@users.noreply.github.com>
2021-06-23 21:23:47 +0200

    Created navbar component with only logo

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import "./Navbar.css"
+
+class Navbar extends Component{
+
+  render(){
+    return(
+<div className="welcomeBanner">
+  <h1 className="welcome-title">MeetApp</h1>
+</div>
+    )
+  }
+}
+export default Navbar
